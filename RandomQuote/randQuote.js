dbed77406b5f0f6d8983c2628d4f0d46b76b542a
Sam <climbinsheep@gmail.com>
2017-01-10 12:00:35 +0000

    adding api quote functionality

--- RandomQuote/randQuote.js
+++ RandomQuote/randQuote.js
@@ -1,11 +1,25 @@
-var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=xml&lang=en',
-    xhttp = new XMLHttpRequest();
-
-
-function newQuote() {
-    if (xhttp) {
-        xhttp.open('GET', url, true);
-        
+$(document).ready(function () {
+    
+    "use strict";
+    function newQuote() {
+        var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
+        $.getJSON(url, function (data) {
+            $('#quotetext').html(data.quoteText);
+            if (data.quoteAuthor !== "") {
+                $('#authorname').html(data.quoteAuthor);
+            } else {
+                $('#authorname').html("Unknown");
+            }
+        });
     }
-}
-
+    
+    newQuote();
+    
+    $('#tweet').on('click', function () {
+        window.open("https://twitter.com/intent/tweet?text=" + randomQuote);
+    });
+    
+    $('#newQuote').on('click', function () {
+        newQuote();
+    });
+});
