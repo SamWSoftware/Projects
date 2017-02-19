88a4d12b7aec5c21c93d73f2a398406e87e57dfa
Sam Williams <climbinsheep@gmail.com>
2017-02-19 20:05:31 +0000

    adding Simon game and update ttt

---
+++ simonGame/simon.js
@@ -0,0 +1,150 @@
+var strict = false,
+    clickDisabled = false;
+
+
+function highlight(colour) {
+    'use strict';
+    var pad = document.getElementsByClassName(colour),
+        style = window.getComputedStyle(pad[0]),
+        col = style.getPropertyValue('border'),
+        nums = col.match(/\d+/g),
+        i;
+    nums.shift();
+    /*switch(colour) {
+        case 'c1':
+            //play tune 1
+        case 'c2':
+            //play tune 2
+        case 'c3':
+            //play tune 3
+        case 'c4':
+            //play tune 4
+    }*/
+    for (i = 0; i < 3; i += 1) {
+        nums[i] = Number(nums[i]) + 50;
+    }
+    pad[0].style.border = "10vmin solid rgb(" + nums.join(',') + ')';
+        
+    setTimeout(function () {
+        pad[0].style.border = "10vmin solid " + col.slice(11);
+    }, 400);
+}
+function run(sequence) {
+    'use strict';
+    var i = 0,
+        show;
+    sequence.push('c' + Math.ceil(Math.random() * 4));
+    document.getElementById('screen').innerHTML =  String(sequence.length);
+    show  = setInterval(function () {
+        if (i < sequence.length) {
+            highlight(sequence[i]);
+            i += 1;
+        } else {
+            clearInterval(show);
+        }
+    }, 600);
+    testPlayer(sequence);
+}
+function wrong() {
+    'use strict';
+    var pad = document.getElementsByClassName('face'),
+        style = window.getComputedStyle(pad[0]),
+        col = style.getPropertyValue('background'),
+        nums = col.match(/\d+/g),
+        i;
+    pad[0].style.background = "rgb(255, 0, 0)" + col.slice(19);
+    setTimeout(function () {
+        pad[0].style.background = col;
+    }, 400);
+    if (strict) {
+        run([]);
+    }
+}
+    
+function press(colour) {
+    'use strict';
+    if (!clickDisabled) {
+        highlight(colour);
+        clickDisabled = true;
+        setTimeout(function () {clickDisabled = false; }, 400);
+    }
+    
+}
+function won() {
+    'use strict';
+    document.getElementById('screen').innerHTML = 'Won';
+}
+
+function isCorrect(c, j, seq) {
+    'use strict';
+    if (c !== seq[j]) { // if not the correct button
+        console.log(c, seq[j], j);
+        wrong();
+        if (strict) {
+            run([]);
+        } else {
+            var i = 0;
+            setInterval(function () {
+                if (i < seq.length) {
+                    highlight(seq[i]);
+                    i += 1;
+                }
+            }, 600);
+            return 0;
+        }
+    } else if (j === seq.length - 1) {
+        if (j === 20) {
+            won();
+        } else {
+            run(seq);
+        }
+    }
+    j += 1;
+    return j;
+}
+function testPlayer(seq) {
+    'use strict';
+    var c1 = document.getElementsByClassName('c1'),
+        c2 = document.getElementsByClassName('c2'),
+        c3 = document.getElementsByClassName('c3'),
+        c4 = document.getElementsByClassName('c4'),
+        i = 0;
+    c1[0].onclick = function () {
+        if (!clickDisabled) {
+            highlight('c1');
+            i = isCorrect('c1', i, seq);
+            clickDisabled = true;
+            setTimeout(function () {clickDisabled = false; }, 400);
+        }
+    };
+    c2[0].onclick = function () {
+        if (!clickDisabled) {
+            highlight('c2');
+            i = isCorrect('c2', i, seq);
+            
+            clickDisabled = true;
+            setTimeout(function () {clickDisabled = false; }, 400);
+        }
+    };
+    c3[0].onclick = function () {
+        if (!clickDisabled) {
+            highlight('c3');
+            i = isCorrect('c3', i, seq);
+            clickDisabled = true;
+            setTimeout(function () {clickDisabled = false; }, 400);
+        }
+    };
+    c4[0].onclick = function () {
+        if (!clickDisabled) {
+            highlight('c4');
+            i = isCorrect('c4', i, seq);
+            clickDisabled = true;
+            setTimeout(function () {clickDisabled = false; }, 400);
+        }
+    };
+}
+function strictToggle() {
+    'use strict';
+    strict = strict ? false : true;
+    document.getElementsByClassName('strict')[0].style.background = strict ? 'red' : 'none';
+}
--- tic-tac-toe/ttt.js
+++ tic-tac-toe/ttt.js
@@ -1,22 +1,61 @@
 
 
 function onePlayer() {
+    'use strict';
     choosePeice();
 }
 
-function twoPlayer(){
+function twoPlayer() {
+    'use strict';
     choosePeice();
 }
 
 function choosePeice() {
+    'use strict';
+    var btns = document.getElementById("btns");
+    console.log(btns);
     document.getElementById("question").innerHTML = "Do you want to be crosses or circles?";
-    document.getElementById("btn1").innerHTML = "Crosses";
-    document.getElementById("btn1").onclick = "startGame('x')"
-    document.getElementById("btn2").innerHTML = "Circles";
-    document.getElementById("btn2").onclick = "startGame('o')"
+    btns.innerHTML = '<div class="col-xs-6">                <button onclick="startGame(\'X\')" class="btn btn-block">Crosses</button></div><div class="col-xs-6"><button onclick="startGame(\'O\')" class="btn btn-block">Naught</button></div>';
 }
 
-function startGame(peice) {
-    parent = document.getElementById("screen");
-    parent.innerHTML = "<div"
+function togglePlayer() {
+    'use strict';
+    var p1 = document.getElementById('player1Go'),
+        p2 = document.getElementById('player2Go');
+
+}
+
+function startGame(char) {
+    'use strict';
+    document.getElementById("screen").style.display = 'none';
+    document.getElementById("screen2").style.display = 'block';
+    document.getElementById("player2Go").style.display = 'none';
+    
+    var board = {
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+        5: '',
+        6: '',
+        7: '',
+        8: '',
+        9: ''
+    },
+        winning = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]
+        ],
+    /* Gameplay */
+        sqrs = document.getElementsByClassName('square');
+    document.getElementsByClassName('content').innerHTML = "";
+    
+    
+
 }
